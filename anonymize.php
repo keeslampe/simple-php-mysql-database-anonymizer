3394e064be8e9f63378154a6bdb20f50a4be0aac
Kees Lampe <kees.lampe@gmail.com>
2018-09-03 16:17:53 +0200

    Change database parameters to defaults

--- anonymize.php
+++ anonymize.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("127.0.0.1", "root", "root", "vvn", 7606);
+$mysqli = new mysqli("127.0.0.1", "username", "password", "database", 3306);
 
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (".$mysqli->connect_errno.") ".$mysqli->connect_error;
